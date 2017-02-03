27739f895b7a42b8654c27477d43aeafb62a089a
Wang Dongxu <wangdongxu@4paradigm.com>
2017-02-03 16:36:09 +0800

    solved c++ arrays practice

---
+++ introduction/arrays.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+	int n;
+	scanf("%d", &n);
+	int a[n];
+	for(int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	for(int j = n-1; j >=0; j--) {
+		cout << a[j] << " ";
+	}
+	cout << endl;
+    return 0;
+}
+
